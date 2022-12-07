1a2f5c0805c2e30f7f1a49522dfbdc49b8a381a0
Morgován Nimród <morgovannimrod22@gmail.com>
2022-12-07 13:00:45 +0100

    year szerinti szűrés megírása + editorconfig hozzáadása

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Get, Controller, Render } from '@nestjs/common';
+import { Get, Controller, Render, Query } from '@nestjs/common';
 import db from './db';
 
 @Controller()
 export class AppController {
   @Get()
   @Render('list')
-  async listpaintings() {
-    const [rows] = await db.execute('SELECT title FROM paintings');
+  async listpaintings(@Query('year') year = 1990) {
+    const [rows] = await db.execute(
+      'SELECT title FROM paintings WHERE year > ?',
+      [year],
+    );
     return {
       paintings: rows,
     };
